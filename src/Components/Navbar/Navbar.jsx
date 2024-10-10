0f20f159efd435898658d2656738c78b929ab4c8
Megha-Praveen <110711257+Megha-Praveen@users.noreply.github.com>
2024-10-10 20:00:57 +0530

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react';
+import { default as React, useRef, useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import logo from '../../assets/logo.png';
+import menu_close from '../../assets/menu_close.svg';
+import menu_open from '../../assets/menu_open.svg';
 import underline from '../../assets/nav_underline.svg';
 import './Navbar.css';
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right="0";
+  }
+
+  const closeMenu = () => {
+    menuRef.current.style.right="-350px";
+  }
 
   return (
     <div className='navbar'>
       <img src={logo} alt="" />
-      <ul className="nav-menu">
+      <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open" />
+      <ul ref={menuRef} className="nav-menu">
+        <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
         <li>
           <AnchorLink className='anchor-link' href='#home'>
             <p onClick={() => setMenu("home")}>Home</p>
