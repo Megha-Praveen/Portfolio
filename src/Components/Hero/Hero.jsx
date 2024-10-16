00d7710164c1880e11c35278fd25181d6c103547
Megha-Praveen <110711257+Megha-Praveen@users.noreply.github.com>
2024-10-16 11:59:44 +0530

    Portfolio V1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
         <img src={profile} alt="no image" />
         <h1><span>I'm Megha Praveen</span></h1>
-        <p>I'm a web developer with a strong background in HTML, CSS, JavaScript, Python, and SQL. <br />I have experience with Django and Flutter, and I'm now learning web development with ME(A)RN.</p>
+        <p>I'm a passionate full stack developer with a strong background in HTML, CSS, JavaScript, ReactJs and SQL. <br />I'm now learning web development with ME(A)RN Stack.</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
